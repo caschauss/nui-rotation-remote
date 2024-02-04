911dd1979b7329606c32b8838b44b4beb612d626
Tristan <tristan.baeumer@stud.hshl.de>
2024-02-05 00:51:40 +0100

    Der flamingo ist gelandet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,29 +3,25 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 var camera, scene, renderer;
-var geometry, material, mesh;
+var geometry, material, mesh, light;
 const loader = new GLTFLoader();
 
 function init() {
-  camera = new THREE.PerspectiveCamera(95, window.innerWidth / window.innerHeight, 1, 1000);
-  camera.position.z = 750;
-
   scene = new THREE.Scene();
-
-  loader.load( './models/Spaceshuttle.glb', function ( gltf ) {
-    mesh = gltf.scene;
+  loader.load("https://ipfs.io/ipfs/QmYqwNYxqmu4z39emTo7h9D62rbwm1esAmbAf2PctAyUvu?filename=Flamingo.glb", function (gltf) {
+    mesh = gltf.scene.children[0];
+    mesh.scale.set(6,6,6);
+    mesh.position.set(0,0,0);
+    light = new THREE.HemisphereLight( 0xbbbbff, 0x444422 );
+    light.position.set( 1, 1, 0 );
+    scene.add( light );
     scene.add(mesh);
   });
 
-  geometry = new THREE.OctahedronGeometry(400, 0);
-  material = new THREE.MeshBasicMaterial({
-    color: 0x0077ff,
-    wireframe: true,
-    wireframeLinewidth: 30
-  });
 
-  //mesh = new THREE.Mesh(geometry, material);
-  //scene.add(mesh);
+  camera = new THREE.PerspectiveCamera(95, window.innerWidth / window.innerHeight, 1, 1000);
+  camera.position.set(0,0,1000);
+  camera.lookAt(0,0,0);
 
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
@@ -45,7 +41,7 @@ function App() {
   animate();
 
   const getOrientationData = () => {
-    fetch("http://192.168.119.125:3001/getData", {
+    fetch("http://192.168.5.156:3001/getData", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ message: "" })
@@ -71,9 +67,11 @@ function App() {
   function animate() {
     requestAnimationFrame(animate);
 
-    mesh.rotation.x = rot1;
-    mesh.rotation.y = rot2;
-    mesh.rotation.z = rot3;
+    if (mesh != null) {
+      mesh.rotation.x = rot1;
+      mesh.rotation.y = rot2;
+      mesh.rotation.z = rot3;
+    }
 
     renderer.render(scene, camera);
   }
